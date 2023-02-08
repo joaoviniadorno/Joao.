28b3feba7e0df37c2cc20369469702a797f6936c
joaoviniadorno <joaoadorno_vf@hotmail.com>
2023-02-07 22:38:05 -0300

    MEDIA PYTHON

---
+++ testr1/print(8+2).py
@@ -0,0 +1,15 @@
+print ( '__________SOME SUA MEDIA_________' )
+
+numero1 = float(input( ' DIGITE SUAS ULTIMAS NOTAS DE PORTUGUES!' )) 
+
+numero2 = float(input( ' DIGITE SUAS ULTIMAS NOTAS DE MATEMATICA!' ))
+
+numero3 = float(input( ' DIGITE SUAS ULTIMAS NOTAS DE CIENCIAS! '))
+
+numero4 = float(input( ' DIGITE SUAS ULTIMAS NOTAS DE ED FISICA! ' ))
+
+soma = (numero1 + numero2 + numero3 + numero4)
+
+soma2 = (soma/4)
+
+print (soma2)
